cb8f3153cce50c4b56fdd37627a7770969475825
Vinicius Rodrigues Rocha <viniciusr.rocha@outlook.com>
2021-01-10 13:39:34 -0300

    Two Way Data Binding - Parte 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,13 +14,16 @@ import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatSelectModule } from '@angular/material/select';
 import { MatDividerModule } from '@angular/material/divider';
+import { TwoWayDataBindingComponent } from './two-way-data-binding/two-way-data-binding.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     StringInterpolationComponent,
     PropertyBindingComponent,
-    EventBindingComponent
+    EventBindingComponent,
+    TwoWayDataBindingComponent
   ],
   imports: [
     BrowserModule,
@@ -31,7 +34,8 @@ import { MatDividerModule } from '@angular/material/divider';
     MatProgressSpinnerModule,
     MatCheckboxModule,
     MatSelectModule,
-    MatDividerModule
+    MatDividerModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/two-way-data-binding/two-way-data-binding.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-two-way-data-binding',
+  templateUrl: './two-way-data-binding.component.html',
+  styleUrls: ['./two-way-data-binding.component.css']
+})
+export class TwoWayDataBindingComponent implements OnInit {
+
+  name1 = '';
+  name2 = '';
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+}
